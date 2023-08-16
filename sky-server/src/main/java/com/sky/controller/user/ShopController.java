28b6f1e9614b5d3889b1a9c055b26819327f7814
shijun <1477525655@qq.com>
2023-08-16 11:26:36 +0800

    day06(微信登录) springboot

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -8,31 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * @BelongsProject: sky-take-out
- * @BelongsPackage: com.sky.controller.admin
- * @Author: ShiJun
- * @CreateTime: 2023-08-14  15:50 设置店铺营业状态
- * @Description: TODO
- * @Version: 1.0
- */
 @RestController("userShopController")
-@Slf4j
-@Api("店铺相关接口")
 @RequestMapping("/user/shop")
-
+@Api(tags = "店铺相关接口")
+@Slf4j
 public class ShopController {
-    private  static  final  String KEY="SHOW_STATUS";
+
+    public static final String KEY = "SHOP_STATUS";
+
     @Autowired
     private RedisTemplate redisTemplate;
 
-
-
- @GetMapping("/status")
- @ApiOperation("获取店铺的营业状态")
- public Result<Integer>getStatus(){
-     Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
-     log.info("获取店铺的营业状态{}",status==1?"营业中":"打样中");
-     return Result.success(status);
- }
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取到店铺的营业状态为：{}",status == 1 ? "营业中" : "打烊中");
+        return Result.success(status);
+    }
 }
